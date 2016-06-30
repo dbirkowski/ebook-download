dd5f150e0fea3663af91ffe405870d336f525ab9
toxicbeaver <toxicbeaver@gmail.com>
2016-07-01 01:57:47 +0200

    final touches

--- app.js
+++ app.js
@@ -1,77 +1,86 @@
+"use strict";
 var request = require('request');
 var cheerio = require('cheerio');
+var fs = require('fs');
+var path = require('path');
+var http = require('http');
 const MAIN_URL = 'http://english.htu.cn/ePub.html';
-console.info(MAIN_URL);
+console.info('starting...');
 request(MAIN_URL, function (error, response, content) {
-    console.info('request works');
+    console.info('request handled');
     if (!error) {
         let $ = cheerio.load(content);
         let authors = $('div > ul > li').slice(2);
+        let data = authors.map((i, el) => {
+            let $author = $(el);
+            let name = $author.text();
 
-        //        var f = $(authors[0]);
-        //        console.log(f.text());
+            let books = $author.next().find('li a');
+            books = books.map((i, el) => {
+                return {
+                    name: getName($(el))
+                    , href: $(el).attr('href')
+                }
+            });
 
-        authors.each(function (el) {
-            let name = $('i', el);
-            console.log(name.text());
+            return {
+                name
+                , books
+            }
         });
 
-        //            let books = el.next('ul').find('li a');
-        //            books = books.map(function (el) {
-        //                return {
-        //                    name: el.text()
-        //                    , href: el.attr('href')
-        //                }
-        //            });
-        //            return {
-        //                name: name
-        //                , books: books
-        //            }
-        //        });
-        //        console.info(authors);
+        downloadBooks(data);
     }
 });
 
+function downloadBooks(data) {
+    data.each((i, el) => {
+        let author = el.name;
+        let books = el.books;
+        mkdirSync(author);
 
-//var http = require('http');
-//var fs = require('fs');
-//
-//var download = function(url, dest, cb) {
-//  var file = fs.createWriteStream(dest);
-//  var request = http.get(url, function(response) {
-//    response.pipe(file);
-//    file.on('finish', function() {
-//      file.close(cb);  // close() is async, call cb after close completes.
-//    });
-//  }).on('error', function(err) { // Handle errors
-//    fs.unlink(dest); // Delete the file async. (But we don't check the result)
-//    if (cb) cb(err.message);
-//  });
-//};
+        books.each((i, el) => {
+            download(el.href, getPath(author, el.name));
+        });
+    });
+}
 
-//var fs    = require('fs')
-//  , path  = require('path');
-//
-//var mkdirSync = function (path) {
-//  try {
-//    fs.mkdirSync(path);
-//  } catch(e) {
-//    if ( e.code != 'EEXIST' ) throw e;
-//  }
-//}
+function getName(name) {
+    let text = name.text();
+    if (text.indexOf(', The') > -1) {
+        text = 'The ' + text.replace(', The', '');
+    }
+    return text;
+}
 
-//var authors = Array.apply(null , document.querySelectorAll('div > ul > li')).slice(2);
-//var authorWithBooks = authors.map(function(el) {
-//    var name = el.textContent;
-//    var books = Array.apply(null , el.nextElementSibling.querySelectorAll('li a'));
-//    books = books.map(function(el) {
-//        return {
-//            name: el.textContent,
-//            href: el.href
-//        }
-//    });
-//    return {
-//        name: name,
-//        books: books
-//    }
-//});
+function getPath(folder, file) {
+    return './' + folder + '/' + file + '.epub';
+}
+
+function mkdirSync(path) {
+    try {
+        fs.mkdirSync(path);
+    } catch (e) {
+        if (e.code != 'EEXIST') throw e;
+    }
+}
+
+function download(url, dest, cb) {
+    fs.access(dest, fs.F_OK, (err) => {
+        if (err) {
+            console.log('downloading ' + dest);
+
+            let file = fs.createWriteStream(dest);
+            let request = http.get(url, function (response) {
+                response.pipe(file);
+                file.on('finish', function () {
+                    console.log('finished ' + dest);
+                    file.close(cb); // close() is async, call cb after close completes.
+                });
+            }).on('error', function (err) { // Handle errors
+                fs.unlink(dest); // Delete the file async. (But we don't check the result)
+                if (cb) cb(err.message);
+            });
+        }
+    });
+};
